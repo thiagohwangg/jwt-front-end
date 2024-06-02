bf5b08c29c86ceec7e333eed8f7ecc6249249280
thiagohwangg <cmnqpart2@gmail.com>
2024-06-02 22:17:22 +0700

    layout with bootstrap 5

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,10 +1,39 @@
-import React from "react";
+import "./Login.scss";
 
 const Login = (props) => {
   return (
-    <div className="login-container">
-        Login
-        <button className="btn btn-danger">Test</button>
+    <div className="login-container mt-3">
+      <div className="container">
+        <div className="row">
+          <div className="content-left col-md-7">
+            <div className="brand">Hoi dan it</div>
+            <div className="detail">
+              Thien Hoang helps you connect and share with the people in your
+              life.
+            </div>
+          </div>
+          <div className="content-right col-md-5 green d-flex flex-column gap-3 py-3">
+            <input
+              className="form-control"
+              placeholder="Email address or phone number"
+              type="text"
+            />
+            <input
+              className="form-control"
+              placeholder="Password"
+              type="password"
+            />
+            <button className="btn btn-primary">Login</button>
+            <span className="text-center">Forgot your password?</span>
+            <br />
+            <div className="text-center">
+              <button className="btn btn-success">
+                Create new account
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
