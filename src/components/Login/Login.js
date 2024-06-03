24f25acaca4793e022e2b2aec673ec1cf1d3f21d
thiagohwangg <cmnqpart2@gmail.com>
2024-06-03 22:53:21 +0700

    lesson 9.3.3 Form Login Responsive

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -2,17 +2,19 @@ import "./Login.scss";
 
 const Login = (props) => {
   return (
-    <div className="login-container mt-3">
+    <div className="login-container">
       <div className="container">
-        <div className="row">
-          <div className="content-left col-7 d-none d-md-block">
+        <div className="row px-3 px-sm-0">
+          <div className="content-left col-12 d-none col-sm-7 d-sm-block">
             <div className="brand">Hoi dan it</div>
             <div className="detail">
               Thien Hoang helps you connect and share with the people in your
               life.
             </div>
           </div>
-          <div className="content-right col-12 col-sm-5 green d-flex flex-column gap-3 py-3">
+          <div className="content-right col-12 col-sm-5 d-flex flex-column gap-3 py-3">
+          <div className="brand d-sm-none">Hoi dan it</div>
+            
             <input
               className="form-control"
               placeholder="Email address or phone number"
@@ -24,12 +26,14 @@ const Login = (props) => {
               type="password"
             />
             <button className="btn btn-primary">Login</button>
-            <span className="text-center">Forgot your password?</span>
+            <span className="text-center">
+              <a href="#" className="forgot-password">
+                Forgot your password?
+              </a>
+            </span>
             <br />
             <div className="text-center">
-              <button className="btn btn-success">
-                Create new account
-              </button>
+              <button className="btn btn-success">Create new account</button>
             </div>
           </div>
         </div>
