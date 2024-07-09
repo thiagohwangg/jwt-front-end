8865333a81af5221ae75ad4bf8d86217465d55fc
Thien Hoang <cmnqpart2@gmail.com>
2024-07-09 23:03:37 +0700

    lesson 12.4  Add Roles

--- src/components/Role/Role.js
+++ src/components/Role/Role.js
@@ -2,33 +2,68 @@ import React, { useEffect, useState } from "react";
 import _ from "lodash";
 import { v4 as uuidv4 } from "uuid";
 import "./Role.scss";
+import { toast } from "react-toastify";
+import {createRoles} from "../../services/roleService"
 const Role = () => {
+  const dataChildDefault = { url: "", description: "", isValidUrl: true };
   const [listChild, setListChild] = useState({
-    child1: { url: "", description: "" },
+    child1: dataChildDefault,
   });
 
   const handleOnChangeInput = (name, value, key) => {
     let _listChild = _.cloneDeep(listChild);
     _listChild[key][name] = value;
+
+    if (value && name === "url") {
+      _listChild[key]["isValidUrl"] = true;
+    }
     setListChild(_listChild);
   };
 
   const handleAddNewInput = () => {
     let _listChild = _.cloneDeep(listChild);
-    _listChild[`child-${uuidv4()}`] = {
-      url: "",
-      description: "",
-    };
-
+    _listChild[`child-${uuidv4()}`] = dataChildDefault;
     setListChild(_listChild);
   };
 
   const handleDeleteInput = (key) => {
     let _listChild = _.cloneDeep(listChild);
     delete _listChild[key];
-    setListChild(_listChild)
+    setListChild(_listChild);
+  };
 
+  const buildDataToPersist = () => {
+    let _listChild = _.cloneDeep(listChild);
+    let result = [];
+    Object.entries(listChild).find(([key, child], index) => {
+      result.push({
+        url: child.url,
+        description: child.description
+      })
+    });
+    return result
   }
+
+  const handleSave = async() => {
+    let invalidObj = Object.entries(listChild).find(([key, child], index) => {
+      return child && !child.url;
+    });
+    if (!invalidObj) {
+      // call api
+      let data= buildDataToPersist()
+      let res = await createRoles(data)
+      if(res?.EC === 0) {
+        toast.success(res?.EM)
+      }
+    } else {
+      //err
+      toast.error("Input URL must not be empty..")
+      let _listChild = _.cloneDeep(listChild);
+      const key = invalidObj[0];
+      _listChild[key]["isValidUrl"] = false;
+      setListChild(_listChild);
+    }
+  };
 
   return (
     <div className="role-container">
@@ -48,7 +83,9 @@ const Role = () => {
                         handleOnChangeInput("url", e.target.value, key)
                       }
                       type="text"
-                      className="form-control"
+                      className={`form-control ${
+                        child.isValidUrl ? "" : "is-invalid"
+                      }`}
                       value={child.url}
                     />
                   </div>
@@ -71,7 +108,10 @@ const Role = () => {
                       <i className="fa fa-plus-circle"></i>
                     </button>
                     {index >= 1 && (
-                      <button className="btn btn-danger ms-2" onClick={() => handleDeleteInput(key)}>
+                      <button
+                        className="btn btn-danger ms-2"
+                        onClick={() => handleDeleteInput(key)}
+                      >
                         <i className="fa fa-trash-o"></i>
                       </button>
                     )}
@@ -81,7 +121,9 @@ const Role = () => {
             })}
 
             <div>
-              <button className="btn btn-warning mt-3">Save</button>
+              <button className="btn btn-warning mt-3" onClick={handleSave}>
+                Save
+              </button>
             </div>
           </div>
         </div>
